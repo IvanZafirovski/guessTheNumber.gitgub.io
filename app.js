40ba823edf62000a95ce7ae6c48d4257e02a7355
Ivan Zafirovski <ivan_zafirovski@yahoo.com>
2023-02-28 22:54:55 +0100

    add ternary operator & clener code

--- app.js
+++ app.js
@@ -4,18 +4,26 @@ console.log(secretNumber);
 let score = 20;
 let highScore = 0;
 
+const messagePrint = function (message) {
+  document.querySelector(".message").textContent = message;
+};
+const numberPrint = function (number) {
+  document.querySelector(".number").textContent = number;
+};
+
 // document.querySelector(".number").textContent = secretNumber;
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
 
   if (!guess) {
-    document.querySelector(".message").textContent =
-      "💥 Please enter number between 1 & 20";
+    messagePrint("💥 Please enter number between 1 & 20");
 
     //when is correct
   } else if (guess === secretNumber) {
-    document.querySelector(".message").textContent = " 🎉 Correct Number 🎉";
-    document.querySelector(".number").textContent = secretNumber;
+    // document.querySelector(".message").textContent = " 🎉 Correct Number 🎉";
+    messagePrint("🎉 Correct Number 🎉");
+    // document.querySelector(".number").textContent = secretNumber;
+    numberPrint(secretNumber);
     document.querySelector("body").style.backgroundColor = "#228B22";
     document.querySelector(".guess").style.backgroundColor = "#FFFFFF";
     document.querySelector(".guess").style.color = "#000000";
@@ -25,23 +33,11 @@ document.querySelector(".check").addEventListener("click", function () {
       highScore = score;
       document.querySelector(".highscore").textContent = highScore;
     }
-
-    //when is higher
-  } else if (guess > secretNumber) {
+    //when the guess is wrong
+  } else if (guess !== secretNumber) {
     if (score > 1) {
-      document.querySelector(".message").textContent = " ⬆️ Too HIGH ⬆️";
-      score--;
-      document.querySelector(".score").textContent = score;
-    } else {
       document.querySelector(".message").textContent =
-        "😔 You lost the game 😔";
-      document.querySelector(".score").textContent = 0;
-    }
-
-    //when is lower
-  } else if (guess < secretNumber) {
-    if (score > 1) {
-      document.querySelector(".message").textContent = "⬇️ Too LOW ⬇️";
+        guess > secretNumber ? " ⬆️ Too HIGH ⬆️" : "⬇️ Too LOW ⬇️";
       score--;
       document.querySelector(".score").textContent = score;
     } else {
@@ -50,6 +46,30 @@ document.querySelector(".check").addEventListener("click", function () {
       document.querySelector(".score").textContent = 0;
     }
   }
+  //   //when is higher
+  // } else if (guess > secretNumber) {
+  //   if (score > 1) {
+  //     document.querySelector(".message").textContent = " ⬆️ Too HIGH ⬆️";
+  //     score--;
+  //     document.querySelector(".score").textContent = score;
+  //   } else {
+  //     document.querySelector(".message").textContent =
+  //       "😔 You lost the game 😔";
+  //     document.querySelector(".score").textContent = 0;
+  //   }
+
+  //   //when is lower
+  // } else if (guess < secretNumber) {
+  //   if (score > 1) {
+  //     document.querySelector(".message").textContent = "⬇️ Too LOW ⬇️";
+  //     score--;
+  //     document.querySelector(".score").textContent = score;
+  //   } else {
+  //     document.querySelector(".message").textContent =
+  //       "😔 You lost the game 😔";
+  //     document.querySelector(".score").textContent = 0;
+  //   }
+  // }
 });
 document.querySelector(".again").addEventListener("click", function () {
   console.log("click");
@@ -57,7 +77,7 @@ document.querySelector(".again").addEventListener("click", function () {
   secretNumber = Math.trunc(Math.random() * 20) + 1;
   document.querySelector(".score").textContent = score;
   document.querySelector(".number").textContent = "?";
-  document.querySelector(".message").textContent = "Start guesing...";
+  messagePrint("Start guesing...");
   document.querySelector("body").style.backgroundColor = "#000000";
   document.querySelector(".guess").style.backgroundColor = "#000000";
   document.querySelector(".guess").style.color = "#ffffff";
